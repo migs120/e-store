bfe78aed563a3ff470dce00c600cc9737f784108
Your Name <you@example.com>
2016-06-28 23:02:15 +0000

    newCrudSimple

--- app/controllers/m_category_controller.rb
+++ app/controllers/m_category_controller.rb
@@ -7,7 +7,17 @@ class MCategoryController < ApplicationController
 
   def new
   end
+  
+   def create
+    MCategory.create(title: params[:m_category][:title] ,body: params[:m_category][:body])
+    redirect_to root_path
+  end
 
   def edit
+  end
+  
+  def update
+    MCategory.find(params[:id]).update_attributes(title: params[:m_category][:title] ,body: params[:m_category][:body] )
+    redirect_to m_category_path(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   
   get 'about' => 'welcome#about'
   
-  root to: 'welcome#index'
+  root to: 'm_category#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
