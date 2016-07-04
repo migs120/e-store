c6df6bc5dbcf69a57f5d36768999102956e5ee9a
Your Name <migs432@yahoo.com>
2016-07-04 20:54:53 +0000

    added create pic logic for items

--- app/controllers/item_pics_controller.rb
+++ app/controllers/item_pics_controller.rb
@@ -7,6 +7,11 @@ class ItemPicsController < ApplicationController
 
   def new
   end
+  
+  def create
+    ItemPic.create(pic_url: params[:item_pic][:pic_url], item_id: params[:item_id] )
+    redirect_to main_category_item_path(params[:main_category_id], params[:item_id])
+  end  
 
   def edit
   end
