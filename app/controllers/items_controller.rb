a22503a1acb36914a138e8d06084b755064fa80c
Your Name <migs432@yahoo.com>
2016-07-05 02:11:57 +0000

    added back links and forms for new items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,12 @@ class ItemsController < ApplicationController
 
   def new
   end
+  
+  def create
+    @item = Item.create(title: params[:item][:title] ,body: params[:item][:body], main_category_id: params[:main_category_id])
+    @item_id = @item.id
+    redirect_to main_category_item_path(@item.main_category_id,@item.id)
+  end
 
   def edit
   end
