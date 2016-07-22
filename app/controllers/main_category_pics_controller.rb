82caefc9a0192301c0c27b98b0114ad2be01f2ae
Your Name <migs432@yahoo.com>
2016-07-22 14:27:20 +0000

    added columns to picture models and additional forms for pics

--- app/controllers/main_category_pics_controller.rb
+++ app/controllers/main_category_pics_controller.rb
@@ -10,7 +10,7 @@ class MainCategoryPicsController < ApplicationController
   
   def create
    
-    MainCategory.find(params[:main_category_id]).main_category_pics.create(params.require(:main_category_pic).permit(:pic_url))
+    MainCategory.find(params[:main_category_id]).main_category_pics.create(params.require(:main_category_pic).permit(:pic_url, :name, :description))
     redirect_to root_path
   end
 
@@ -19,7 +19,7 @@ class MainCategoryPicsController < ApplicationController
   
   def update
    
-    MainCategoryPic.find(params[:id]).update_attributes(params.require(:main_category_pic).permit(:pic_url) )
+    MainCategoryPic.find(params[:id]).update_attributes(params.require(:main_category_pic).permit(:pic_url, :name, :description) )
     redirect_to main_category_path(MainCategoryPic.find(params[:id]).main_category_id )
   end
   
