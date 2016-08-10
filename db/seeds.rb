b360ffe31bc561e4cc9aa8547a70c05d5bf23140
Your Name <migs432@yahoo.com>
2016-08-10 18:48:44 +0000

    seed generator working!! but teplates still need configuring to display urlseed for itempics

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,16 @@ require 'faker'
  # The `save` method then saves this User to the database.
 
  # Create an admin user
-cat1x = MainCategory.new(title: "first categroy", body: "first category description", template: false) 
+cat3x = MainCategory.new(title: "this is a template category", body: "this is template body description", template: true) 
 
-cat1x.main_category_pics.build(pic_url:"https://bloccit-120.s3.amazonaws.com/uploads/main_category_pic/pic_url/3/ScreenHunter_04_Jun._08_17.19.gif", name: "", urlseed: "https://bloccit-120.s3.amazonaws.com/uploads/main_category_pic/pic_url/3/ScreenHunter_04_Jun._08_17.19.gif")
-cat1x.items.build(title:"the first item title", name: "the first item name", body: "this is the first item body", price: 3.0, active: true )
-cat1x.items.build(title:"second item title", name: "secont item name", body: "second item body", price: 2.0, active: true )
-cat1x.save!
-puts cat1x.inspect
+cat3x.main_category_pics.build(pic_url:"https://bloccit-120.s3.amazonaws.com/uploads/main_category_pic/pic_url/1/ScreenHunter_03_Jun._07_20.03.gif", name: "template", urlseed: "https://bloccit-120.s3.amazonaws.com/uploads/main_category_pic/pic_url/1/ScreenHunter_03_Jun._07_20.03.gif")
+cat3x.main_category_pics.build(pic_url:"https://bloccit-120.s3.amazonaws.com/uploads/main_category_pic/pic_url/2/ScreenHunter_04_Jun._08_17.19.gif", name: "", urlseed: "https://bloccit-120.s3.amazonaws.com/uploads/main_category_pic/pic_url/2/ScreenHunter_04_Jun._08_17.19.gif")
+cat3x.save!
+cat3x3x = cat3x.items.build(title:"second item title", name: "secont item name", body: "this is second item body", price: 1.0, active: true )
+cat3x3x.save!
+Item.find(cat3x3x.id).item_pics.build(pic_url:"https://bloccit-120.s3.amazonaws.com/uploads/item_pic/pic_url/2/ScreenHunter_02_Jun._07_20.01.gif", name: "template", urlseed: "https://bloccit-120.s3.amazonaws.com/uploads/item_pic/pic_url/2/ScreenHunter_02_Jun._07_20.01.gif").save!
+Item.find(cat3x3x.id).item_pics.build(pic_url:"https://bloccit-120.s3.amazonaws.com/uploads/item_pic/pic_url/3/ScreenHunter_04_Jun._08_17.19.gif", name: "", urlseed: "https://bloccit-120.s3.amazonaws.com/uploads/item_pic/pic_url/3/ScreenHunter_04_Jun._08_17.19.gif").save!
 
-
-
-
-puts cat1x.id
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
